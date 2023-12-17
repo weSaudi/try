fb7d872f065fc25af1f25d69d5a4fea6a6de7303
weSaudi <129343229+weSaudi@users.noreply.github.com>
2023-12-17 04:34:21 -0800

    Add files via upload

---
+++ story_content/user.js
@@ -0,0 +1,45 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5YzbLTX9LKO":
+        Script1();
+        break;
+      case "5X6E6FxHAtK":
+        Script2();
+        break;
+  }
+}
+
+function Script1()
+{
+  var player = GetPlayer();
+
+src="https://code.jquery.com/jquery-3.7.0.js";
+
+$(function()
+{
+    $.getJSON( "one.json", function(data) {
+        player.SetVar("txt", data[0]["one"]);
+        console.log( data );
+        console.log( myjsonserver );
+      })
+})
+}
+
+function Script2()
+{
+  src="https://code.jquery.com/jquery-3.7.0.js";
+
+$(function()
+{
+    $.getJSON( "one.json", function(data) {
+        data[0]["one"]="ramy";
+        const myjsonserver = JSON.stringify(data);
+        console.log( data );
+        console.log( myjsonserver );
+      })
+})
+
+}
+
